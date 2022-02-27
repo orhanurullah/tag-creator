b942867c8e125adf39c1c230e7a98cc1e1c52d45
orhanurullah <orhann.1512@gmail.com>
2022-02-28 00:14:21 +0300

    Tag Creator Created

---
+++ README.md
@@ -0,0 +1,15 @@
+# Add Element In HTML Page
+Write tag name in the input element after then select one of the buttons.   
+
+ If you want to add as a last click to the 'Add to Last' buton. 
+ If you want to add as a first click to the 'Add to First' buton.
+            
+For delete all elements click to the 'All Remove' buton
+            
+For delete first element click to the 'Remove from First' buton
+            
+For delete last element click to the 'Remove from Last' buton
+
+For delete all stroke elements click to the 'Only Stroke Elements Remove buton
+
+For delete one of any elements right click 
---
+++ index.js
@@ -0,0 +1,85 @@
+const todo = document.getElementById("todo");
+const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
+const button5 = document.getElementById('button5');
+const button6 = document.getElementById('button6');
+
+const data = document.getElementById('input');
+let a = 0;
+// Create P Element
+const createElement = (type) => {
+    let elementName = data.value;
+    if(!elementName){
+        return null;
+    }
+    let element = document.createElement(elementName);
+    element.classList.add(elementName+""+a);
+    element.setAttribute('id', elementName+""+(a++));
+    if(type === 1){
+        todo.appendChild(element);
+    }else{
+        todo.insertBefore(element, todo.firstChild);
+    }
+    element.innerText = elementName + " elementi eklendi. Element No: " + a;
+    element.style.cursor="pointer";
+    element.addEventListener('dblclick',function(){
+       let val = element.innerText;
+       element.innerHTML = "";
+       let del = document.createElement('del');
+       del.setAttribute('id', 'del' + a);
+       element.appendChild(del);
+       del.innerText = val;
+    });
+    element.addEventListener('contextmenu', deleteSingleElement);
+}
+
+//Delete Single Element
+const deleteSingleElement = () => {
+    event.preventDefault();
+    todo.removeChild(event.target);
+}
+
+//Delete All P Elements Function
+const AllElementsRemove = () => {
+    a = 0;
+    while(todo.hasChildNodes()){
+       todo.removeChild(todo.firstChild);
+    }
+};
+// Delete Only Text Stroke Elements Function
+const onlyStrokeElementsRemove = () => {
+   let dels = document.getElementsByTagName('del');
+   let elements = new Array();
+   for(let i = 0; i < dels.length; i++){
+        elements.push(dels[i].parentElement);
+   }
+   elements.map(e => e.remove())
+};
+
+// Delete From First Function
+const deleteElement = (type) => {
+    if(type === 1){
+        todo.removeChild(todo.firstChild);
+    }else{
+        todo.removeChild(todo.lastChild);
+    }
+};
+
+/**
+ * Listeners
+ */
+
+//Add Element to Last
+button.addEventListener('click', () => createElement(1));
+//Add Element to First
+button4.addEventListener('click', () => createElement(0));
+//Delete All Elements 
+button2.addEventListener('click', AllElementsRemove);
+//Delete First Element
+button5.addEventListener('click', () => deleteElement(1));
+//Delete Last Element
+button6.addEventListener('click', () => deleteElement(0));
+//Delete Only Text Stroke Elements
+button3.addEventListener('click', onlyStrokeElementsRemove);
